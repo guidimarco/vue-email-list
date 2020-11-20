e0f5489b0ae9ec318e0c6f2f7d969ff8dddd3e91
Marco Guidi <mark.88g@gmail.com>
2020-11-20 14:19:52 +0100

    CDN axios e API mail boolean

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,14 @@ var app = new Vue({ // VUE INSTANCE
     computed: {
 
     },
-    methods: { // for function
-
+    mounted() {
+        axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then( (response) => {
+                console.log(response);
+                console.log(response.data);
+                console.log(response.data.response);
+            })
+        ; // END AXIOS
     },
 });
