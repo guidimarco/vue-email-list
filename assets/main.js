99d850ee923df4153374866458dbe932eda2a28a
Marco Guidi <mark.88g@gmail.com>
2020-11-20 14:08:57 +0100

    HTML-CSS-JS primo push

---
+++ assets/main.js
@@ -0,0 +1,16 @@
+var app = new Vue({ // VUE INSTANCE
+    el: "#root",
+    data: {
+        // VAR
+
+        // OBJ
+
+        // ARRAY
+    },
+    computed: {
+
+    },
+    methods: { // for function
+
+    },
+});
